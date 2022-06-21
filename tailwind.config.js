14833e4c3f79e65e4306e757390011b08ebe15c7
aliiev <aliiev.akhtem@gmail.com>
2022-06-21 22:29:22 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [ './src/**/*.js', './public/index.html' ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': [ 'Inter', ...defaultTheme.fontFamily.sans ]
+      }
+    },
+  },
+  plugins: [],
+}
